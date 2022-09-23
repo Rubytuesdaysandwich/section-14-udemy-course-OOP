59d3fc3b2e458600cbf69a8385e5b8236386c875
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-09-23 11:55:20 -0600

    section 14 udemy course starting OOP

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,53 @@
 'use strict';
+//OOP object oriented programming
+//Constructor Functions and the new Operator
+//constructor functions should start with capital letters
+const Person = function (firstName, birthYear) {
+  //instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  console.log(this);
+
+  //never do this//the object will be stuck carrying this function and upon each user creation a new method will be made as well.
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+//1.New {} is created
+//2.function is called, this = {}
+//3. {} linked to prototype
+//4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+//!========
+//-----prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+//.prototypeOfLinkedObjects
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+//-----prototypes
+//!========
