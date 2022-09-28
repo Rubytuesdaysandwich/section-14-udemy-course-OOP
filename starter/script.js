8c370bce02ed1d2b383937ed481a6d345b77dd2d
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-09-28 15:58:19 -0600

    update end of another class example

--- starter/script.js
+++ starter/script.js
@@ -465,9 +465,31 @@ class Account {
     console.log(`Thanks for opening an account, ${owner}
     `);
   }
+  //public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
 }
 const acc1 = new Account('Jonas', 'EUR', 1111);
 console.log(acc1);
-
-acc1.movements;
+//// acc1._movements.push(250);
+//// acc1._movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1);
+console.log(acc1.pin);
 //end another class example
